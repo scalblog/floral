4adb668e5d1d07def747c5edafc97a64452db205
scalblog <scalblog@yahoo.fr>
2017-12-10 15:06:50 +0100

    new containers

--- index.php
+++ index.php
@@ -14,95 +14,145 @@
     <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
   </head>
   <body>
-    <div class="container">
-        <header>
-            <div class="row top_head_banner">
-                <div class="col-sm-4">
-                    <a href="mailto:contact@email.com" >Nous écrire <i class="fa fa-envelope-o" aria-hidden="true"></i> contact@email.com</a>
-                </div>
-                <div class="col-sm-4">
-                    <a href="https://www.google.fr/maps/place/12+Rue+Dauphine,+75006+Paris/@48.8558069,2.3378829,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671df5b6ec5a1:0x8a3a3e574d974c9b!8m2!3d48.8558034!4d2.3400769" target="_blank">Se rendre au <i class="fa fa-map-maker" aria-hidden="true"></i> 12 rue Dauphine, Paris 6e</a>
-                </div>
-                <div class="col-sm-4">
-                <a href="tel:+33102030405">Nous téléphoner <i class="fa fa-phone" aria-hidden="true"></i> 01 02 03 04 05</a>    
-                </div>
-            </div>
-            <div class="row no-gutters">
-                <div class="col-sm-3">
-                    <p>Logo Flowers</p>
-                </div>
-                <div class="col-sm-9">
-                    <nav>
-                        <a href="">Accueil</a>
-                        <a href="">Services</a>
-                        <a href="">Notre histoire</a>
-                        <a href="">Boutique</a>
-                        <a href="">Contact</a>                        
-                    </nav>
+      <header>
+            <div class="top_head_banner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <a href="mailto:contact@email.com" >Nous écrire <i class="fa fa-envelope-o" aria-hidden="true"></i> contact@email.com</a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="https://www.google.fr/maps/place/12+Rue+Dauphine,+75006+Paris/@48.8558069,2.3378829,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671df5b6ec5a1:0x8a3a3e574d974c9b!8m2!3d48.8558034!4d2.3400769" target="_blank">Se rendre au <i class="fa fa-map-maker" aria-hidden="true"></i> 12 rue Dauphine, Paris 6e</a>
+                        </div>
+                        <div class="col-sm-4">
+                        <a href="tel:+33102030405">Nous téléphoner <i class="fa fa-phone" aria-hidden="true"></i> 01 02 03 04 05</a>    
+                        </div>
+                    </div>
                 </div>
             </div>
-        </header>
+            
+            <div class="real-header">
+                <div class="container">
+                    <div class="row no-gutters">
+                        <div class="col-sm-3">
+                            <p>Logo Flowers</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <nav >
+                                <a href="">Accueil</a>
+                                <a href="">Services</a>
+                                <a href="">Notre histoire</a>
+                                <a href="">Boutique</a>
+                                <a href="">Contact</a>                        
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!--
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#">Flowers<img src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-social-logo.png" width="30" height="30" class="d-inline-block align-top" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Notre Histoire</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Boutique</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+   
+                </div>
+            </nav>
+            -->
+        </div>
+    </header>
         <main>
-            <div class="row">
-                <div class="banner">
-                    <div class="presentation">
-                        <p>Vous êtes unique. Comme nos</p>
-                        <h1>Bouquets</h1>
-                        <p>Pour toute occasion.</p>
-                    </div>    
+            <section class="presentation">
+                <div class="container">
+                    <div class="row">
+                        <div class="banner">
+                            <p>Vous êtes unique. Comme nos</p>
+                            <h1>Bouquets</h1>
+                            <p>Pour toute occasion.</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="row concept">
-                <div class="col-sm-4">
-                    <h2>Exclusif</h2>
-                    <p>sera votre bouquet.</p>
-                    <p>Une création originale.</p>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i></p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+            </section>
+            <section class="concept">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h2>Exclusif</h2>
+                            <p>sera votre bouquet.</p>
+                            <p>Une création originale.</p>
+                            <p><i class="fa fa-clock-o" aria-hidden="true"></i></p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h2>Créatif</h2>
+                            <p>est notre état d'esprit.</p>
+                            <p>En permanence.</p>
+                            <p><i class="fa fa-lightbulb-o" aria-hidden="true"></i></p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h2>Imaginatif</h2>
+                            <p>sera le plaisir de l'offrir</p>
+                            <p>accompagné d'un sourire, d'un baiser, d'un Merci.</p>
+                            <p><i class="fa fa-user" aria-hidden="true"></i></p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                        </div>
+                    </div>
+                </div>    
+            </section>
+            <section class="welcome">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <h2>Bienvenue</h2>
+                            <p>Nous vous accueillons au 12 rue Dauphine, Paris 6e.</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h2>Services</h2>
+                            <ul>
+                                <li>Célébrations</li>
+                                <li>Mariages</li>
+                                <li>Evénements</li>
+                                <li>Cérémonies</li>
+                                <li>Plaisir d'offrir</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-4">
-                    <h2>Créatif</h2>
-                    <p>est notre état d'esprit.</p>
-                    <p>En permanence.</p>
-                    <p><i class="fa fa-lightbulb-o" aria-hidden="true"></i></p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
-                </div>
-                <div class="col-sm-4">
-                    <h2>Imaginatif</h2>
-                    <p>sera le plaisir de l'offrir</p>
-                    <p>accompagné d'un sourire, d'un baiser, d'un Merci.</p>
-                    <p><i class="fa fa-user" aria-hidden="true"></i></p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-8">
-                    <h2>Bienvenue</h2>
-                    <p>Nous vous accueillons au 12 rue Dauphine, Paris 6e.</p>
-                </div>
-                <div class="col-sm-4">
-                    <h2>Services</h2>
-                    <ul>
-                        <li>Célébrations</li>
-                        <li>Mariages</li>
-                        <li>Evénements</li>
-                        <li>Cérémonies</li>
-                        <li>Plaisir d'offrir</li>
-                    </ul>
-                </div>
-            </div>
+            </section>    
         </main>
         <footer>
-            <div class="row">
-                <div class="col-12 col-sm-4">
-                    <p>Notre boutique se situe</p>
-                    <p>12 rue Dauphine, Paris 6e</p>
-                </div>
-                <div class="col-12 col-sm-4">
-                    <p>Texte</p>
-                </div>
-                <div class="col-12 col-sm-4">
-                    <p>Texte</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-4">
+                        <p>Notre boutique se situe</p>
+                        <p>12 rue Dauphine, Paris 6e</p>
+                    </div>
+                    <div class="col-12 col-sm-4">
+                        <p>Texte</p>
+                    </div>
+                    <div class="col-12 col-sm-4">
+                        <p>Texte</p>
+                    </div>
                 </div>
             </div>
         </footer>
---
+++ index3.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>Votre fleuriste</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=News+Cycle|Signika" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+  </head>
+  <body>
+    <div class="container">
+        <header>
+            <div class="row top_head_banner">
+                <div class="col-sm-4">
+                    <a href="mailto:contact@email.com" >Nous écrire <i class="fa fa-envelope-o" aria-hidden="true"></i> contact@email.com</a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="https://www.google.fr/maps/place/12+Rue+Dauphine,+75006+Paris/@48.8558069,2.3378829,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671df5b6ec5a1:0x8a3a3e574d974c9b!8m2!3d48.8558034!4d2.3400769" target="_blank">Se rendre au <i class="fa fa-map-maker" aria-hidden="true"></i> 12 rue Dauphine, Paris 6e</a>
+                </div>
+                <div class="col-sm-4">
+                <a href="tel:+33102030405">Nous téléphoner <i class="fa fa-phone" aria-hidden="true"></i> 01 02 03 04 05</a>    
+                </div>
+            </div>
+            
+            <div class="row no-gutters real-header">
+                <div class="col-sm-3">
+                    <p>Logo Flowers</p>
+                </div>
+                <div class="col-sm-9">
+                    <nav >
+                        <a href="">Accueil</a>
+                        <a href="">Services</a>
+                        <a href="">Notre histoire</a>
+                        <a href="">Boutique</a>
+                        <a href="">Contact</a>                        
+                    </nav>
+                </div>
+            </div>
+            
+            <!--
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#">Flowers<img src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-social-logo.png" width="30" height="30" class="d-inline-block align-top" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Notre Histoire</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Boutique</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+   
+                </div>
+            </nav>
+            -->
+
+        </header>
+        <main>
+            <div class="row">
+                <div class="banner">
+                    <div class="presentation">
+                        <p>Vous êtes unique. Comme nos</p>
+                        <h1>Bouquets</h1>
+                        <p>Pour toute occasion.</p>
+                    </div>    
+                </div>
+            </div>
+            <div class="row concept">
+                <div class="col-sm-4">
+                    <h2>Exclusif</h2>
+                    <p>sera votre bouquet.</p>
+                    <p>Une création originale.</p>
+                    <p><i class="fa fa-clock-o" aria-hidden="true"></i></p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                </div>
+                <div class="col-sm-4">
+                    <h2>Créatif</h2>
+                    <p>est notre état d'esprit.</p>
+                    <p>En permanence.</p>
+                    <p><i class="fa fa-lightbulb-o" aria-hidden="true"></i></p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                </div>
+                <div class="col-sm-4">
+                    <h2>Imaginatif</h2>
+                    <p>sera le plaisir de l'offrir</p>
+                    <p>accompagné d'un sourire, d'un baiser, d'un Merci.</p>
+                    <p><i class="fa fa-user" aria-hidden="true"></i></p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <h2>Bienvenue</h2>
+                    <p>Nous vous accueillons au 12 rue Dauphine, Paris 6e.</p>
+                </div>
+                <div class="col-sm-4">
+                    <h2>Services</h2>
+                    <ul>
+                        <li>Célébrations</li>
+                        <li>Mariages</li>
+                        <li>Evénements</li>
+                        <li>Cérémonies</li>
+                        <li>Plaisir d'offrir</li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="row">
+                <div class="col-12 col-sm-4">
+                    <p>Notre boutique se situe</p>
+                    <p>12 rue Dauphine, Paris 6e</p>
+                </div>
+                <div class="col-12 col-sm-4">
+                    <p>Texte</p>
+                </div>
+                <div class="col-12 col-sm-4">
+                    <p>Texte</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
