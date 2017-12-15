043f484d7f83e56abe12f99863e305ef38940f04
scalblog <scalblog@yahoo.fr>
2017-12-15 01:24:46 +0100

    test another navbar

---
+++ index4.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>Votre fleuriste</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=News+Cycle|Signika" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/style2.css" media="screen" />
+  </head>
+  <body>
+    <!-- Javascript Page UPsport  -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.11&appId=1805573976119672';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script> 
+    <header>
+        <!--
+        <div class="top_head_banner  sticky-top">
+           <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <a href="mailto:contact@email.com" >Nous écrire <i class="fa fa-envelope-o" aria-hidden="true"></i> contact@email.com</a>
+                    </div>
+                    <div class="col-sm-4">
+                        <a href="https://www.google.fr/maps/place/12+Rue+Dauphine,+75006+Paris/@48.8558069,2.3378829,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671df5b6ec5a1:0x8a3a3e574d974c9b!8m2!3d48.8558034!4d2.3400769" target="_blank">Se rendre au <i class="fa fa-map-maker" aria-hidden="true"></i> 12 rue Dauphine, Paris 6e</a>
+                    </div>
+                    <div class="col-sm-4">
+                        <a href="tel:+33102030405">Nous téléphoner <i class="fa fa-phone" aria-hidden="true"></i> 01 02 03 04 05</a>    
+                    </div>
+                </div>
+            </div>
+        </div>
+        -->
+            
+<!--        <div class="real-header"> -->
+            <!--<div class="container">-->
+            <div><!-- à la place du div class container -->
+                <nav class="navbar navbar-expand-lg another-navbar">
+                <!--<nav class="navbar fixed-top navbar-expand-lg  navbar-light bg-light another-navbar"> -->
+                    <a class="navbar-brand" href="#">Flowers</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Bouquets</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Boutique</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Notre histoire</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#formulaire">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+<!-- fin de la div real-header        </div> -->
+            
+            <!--
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#">Flowers<img src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-social-logo.png" width="30" height="30" class="d-inline-block align-top" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Notre Histoire</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Boutique</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+   
+                </div>
+            </nav>
+            -->
+        
+    </header>
+        <main>
+            <section class="presentation">
+                <div class="container">
+                    <div class="row">
+                        <div class="banner">
+                            <p>Vous êtes unique. Comme nos</p>
+                            <h1>Bouquets</h1>
+                            <p>Pour toute occasion.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="concept">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h2>Exclusif</h2>
+                            <p>sera votre bouquet.</p>
+                            <p>Une création originale.</p>
+                            <p><i class="fa fa-clock-o" aria-hidden="true"></i></p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h2>Créatif</h2>
+                            <p>est notre état d'esprit.</p>
+                            <p>En permanence.</p>
+                            <p><i class="fa fa-lightbulb-o" aria-hidden="true"></i></p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h2>Imaginatif</h2>
+                            <p>sera le plaisir de l'offrir</p>
+                            <p>accompagné d'un sourire, d'un baiser, d'un Merci.</p>
+                            <p><i class="fa fa-user" aria-hidden="true"></i></p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus enim amet maiores, veritatis impedit exercitationem aut explicabo quo placeat doloremque voluptatum! Sunt laboriosam dolor, modi deserunt aspernatur recusandae neque?</p>
+                        </div>
+                    </div>
+                </div>    
+            </section>
+            <section class="welcome">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <h2>Bienvenue</h2>
+                            <p>Nous vous accueillons au 12 rue Dauphine, Paris 6e.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi illo possimus soluta! Distinctio molestiae voluptatem, impedit aut corporis quidem saepe doloremque harum, voluptas magni tenetur recusandae atque itaque voluptate vitae.</p>
+                            <p>Métro : Odéon</p>
+                            <p>Vélib : lorem</p>
+                            <p>Parking privé : ipsum </p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h2>Services</h2>
+                            <ul>
+                                <li>Célébrations</li>
+                                <li>Mariages</li>
+                                <li>Evénements</li>
+                                <li>Cérémonies</li>
+                                <li>Plaisir d'offrir</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>    
+            <section class="instagram">
+                <div class="container">
+                    
+                    <h3 class="text-center">Nos dernières créations (Instagram)</h2>
+                    <p>Envie de commander l'une de nos créations ? <a href="#formulaire">Contactez-nous</a>.</p>
+                    <?php
+                    $insta_url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=317009480.a6bd5b1.0babbdc6fd09421eaa2b7721855a96ee";
+
+
+                    // est ce que Curl est installé ?
+                    /*
+                    if (!function_exists("curl_init")){
+                        die("Désolé cURL n'est pas installé !");
+                    } else {echo "succès curl";}
+                    */
+                   
+
+
+                    
+                    $insta_data = json_decode(file_get_contents('https://api.instagram.com/v1/users/self/media/recent/?access_token=317009480.a6bd5b1.0babbdc6fd09421eaa2b7721855a96ee'));
+                    if($insta_data->meta->code == 200) {
+                        // echo 'Succès token';
+                        
+                        ?>
+                        <div class="col-12 col-sm-10 col-lg-10 text-center">
+                            <img src="<?= $insta_data->data[0]->images->standard_resolution->url ?>" alt="fleuriste-bouquet-1"/>
+                            <img src="<?= $insta_data->data[1]->images->standard_resolution->url ?>" alt="fleuriste-bouquet-2"/>
+                            <img src="<?= $insta_data->data[2]->images->standard_resolution->url ?>" alt="fleuriste-bouquet-3"/>
+                        </div>
+                        <div class="col-12 col-sm-10 col-lg-10 text-center">
+                            <img src="<?= $insta_data->data[3]->images->standard_resolution->url ?>" alt="fleuriste-bouquet-4"/>
+                            <img src="<?= $insta_data->data[4]->images->standard_resolution->url ?>" alt="fleuriste-bouquet-5"/>
+                            <img src="<?= $insta_data->data[5]->images->standard_resolution->url ?>" alt="fleuriste-bouquet-6"/>
+                        </div>
+                        
+                        
+                        
+                        
+                        <?php
+                  
+                        // var_dump($insta_data->data[0]->created_time);
+                        // var_dump($insta_data);
+                        }
+                    
+                    else {
+                        echo 'Impossible de récupérer les images du compte Instagram.';
+                    }
+                    
+                    ?>
+                    <p class="text-center">Découvrez davantage de créations sur notre compte <i class="fa fa-instagram" aria-hidden="true"></i> Instagram.</p>
+
+                </div>
+            </section>
+            <section class="facebook">
+                <div class="container">
+                    <h3 class="text-center facebook-title">Notre actualité (Facebook)</h3>
+                    <p class="espace" >Découvrez nos offres promotionnelles, nos événements, nos créations.</p>
+                    
+                    <div class="row">
+                        <div class="col-12 col-sm-7 col-lg-7">
+                            <!-- Google Maps -->
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.1262245653884!2d2.337888215976662!3d48.85580337928716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671df5b6ec5a1%3A0x8a3a3e574d974c9b!2s12+Rue+Dauphine%2C+75006+Paris!5e0!3m2!1sfr!2sfr!4v1513124392364" width="600" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+                        </div>
+                        <div class="col-12 col-sm-5 col-lg-5">
+                            <!-- Page UpSPort F B s affiche ici -->
+                            <div class="fb-page" data-href="https://www.facebook.com/UpSport.UNis/" data-tabs="timeline" data-width="500" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/UpSport.UNis/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/UpSport.UNis/">UP SPORT Unis pour le Sport</a></blockquote></div>
+                        
+                        </div>
+                    </div>
+
+                    <p class="text-center espace">Retrouvez le reste de notre actualité sur notre page <i class="fa fa-facebook-square" aria-hidden="true"></i> Facebook.</p>
+                </div>
+            </section>
+            <section class="contact" id="formulaire">
+                <h3>La moindre question ? Contactez-nous.</h3>
+	        	<p>Nous vous répondrons avec grand plaisir.</p>
+	        	<form action="contact.php" method="POST">
+		        	<fieldset>
+		              		<input type="texte" placeholder="Votre nom" required="" name="name"><br />
+		        		<input type="email" placeholder="Votre email" required="" name="email">
+		        		<textarea rows="5" cols="50" name="message" class="form-control" id="message" placeholder="Tapez ici votre message..." tabindex="4" required></textarea>
+		        		<button type="submit" class=" espace btn btn-start-order">Envoyer le message <i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+		        	</fieldset>
+	        	</form>
+            </section>
+        </main><!--
+        --><footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-4">
+                        <p>Notre boutique se situe</p>
+                        <p>12 rue Dauphine, Paris 6e</p>
+                    </div>
+                    <div class="col-12 col-sm-4">
+                        <p>Texte</p>
+                    </div>
+                    <div class="col-12 col-sm-4">
+                        <p>Texte</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
