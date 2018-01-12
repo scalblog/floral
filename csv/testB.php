2db17fba09ce496893c01a2093a6edb1e9105c82
scalblog <scalblog@yahoo.fr>
2018-01-12 14:08:46 +0100

    more test csv files

---
+++ csv/testB.php
@@ -0,0 +1,19 @@
+<h1>Test de lecture fichier CSV</h1>
+
+
+<?php
+$row = 1;
+if (($handle = fopen("tips.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        
+        if ($data[2] == 'Female' && $data[3] == 'Yes') {
+            echo( $data[0] * 2 . " instead of ". $data[0]. " because she's smoking.<br />\n");
+        }
+
+        $row++;
+        
+        
+    }
+    fclose($handle);
+}
+?>
