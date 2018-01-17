57664d53dec124d4d1a6e02821105618fd33a0d2
scalblog <scalblog@yahoo.fr>
2018-01-17 13:13:28 +0100

    money csv file

---
+++ csv/money.php
@@ -0,0 +1,17 @@
+<h1>Test de lecture fichier CSV</h1>
+
+
+<?php
+$row = 1;
+if (($handle = fopen("money.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num champs à la ligne $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
+?>
